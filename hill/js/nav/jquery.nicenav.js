63f7e5ae993fa59d81f17745ec909c31b1fbd38b
case <1273680599@qq.com>
2016-10-24 13:28:44 +0800

    caseTwo

---
+++ hill/js/nav/jquery.nicenav.js
@@ -0,0 +1,17 @@
+(function ($) {
+    $.extend({
+        'nicenav': function (con) {
+            con = typeof con === 'number' ? con : 400;
+            var $lis = $('#nav>li'), $h = $('#buoy');
+            $lis.hover(function () {
+                $h.stop().animate({
+                    'left': $(this).offsetParent().context.offsetLeft
+                }, con);
+            }, function () {
+                $h.stop().animate({
+                    'left': '20'
+                }, con);
+            })
+        }
+    });
+}(jQuery));
